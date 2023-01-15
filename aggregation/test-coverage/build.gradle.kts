60292bdb3a2fdef8530492456cd9f800b6d1e504
zhaojj11 <zhaojj11@zhaojj11.com>
2023-01-16 00:10:27 +0800

    build: :building_construction: 加入sample项目 (#1)

---
+++ aggregation/test-coverage/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.zhaojj11.jam.report-aggregation")
+}
+
+
+dependencies {
+    aggregate("com.zhaojj11.jam.apps:sample")
+}
---
+++ apps/sample/src/main/java/com/zhaojj11/jam/sample/SampleApplication.java
@@ -0,0 +1,16 @@
+package com.zhaojj11.jam.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * sample 启动器
+ *
+ * @author zhaojj11
+ */
+@SpringBootApplication
+public class SampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+}
