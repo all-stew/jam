233a870c86ac745b841561ecf1b0c2554fac13a4
zhaojj11 <zhaojj11@zhaojj11.com>
2023-01-17 00:10:28 +0800

    build: :building_construction: 优化circleci

--- aggregation/test-coverage/build.gradle.kts
+++ aggregation/test-coverage/build.gradle.kts
@@ -1,16 +1,6 @@
 plugins {
     id("com.zhaojj11.jam.report-aggregation")
-    id("org.sonarqube") version "3.5.0.2730"
 }
-
-sonarqube {
-    properties {
-        property("sonar.projectKey", "all-stew_jam")
-        property("sonar.organization", "all-stew")
-        property("sonar.host.url", "https://sonarcloud.io")
-    }
-}
-
 
 dependencies {
     aggregate("com.zhaojj11.jam.apps:sample")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,12 @@
-// EMPTY now
+plugins {
+    id("jacoco")
+    id("org.sonarqube") version ("3.5.0.2730")
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "all-stew_jam")
+        property("sonar.organization", "all-stew")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
