e1b60deb842025f80eb140a096d791250247a514
zhaojj11 <zhaojj11@zhaojj11.com>
2023-01-18 01:02:08 +0800

    build: :building_construction: ...

--- apps/sample/build.gradle.kts
+++ apps/sample/build.gradle.kts
@@ -1,6 +1,15 @@
 plugins {
     id("jacoco")
     id("com.zhaojj11.jam.spring-boot-application")
+    id("org.sonarqube") version ("3.5.0.2730")
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "all-stew_jam")
+        property("sonar.organization", "all-stew")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
 
 group = "${group}.apps"
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,10 @@
-plugins {
-    id("org.sonarqube") version ("3.5.0.2730")
-}
-
-sonarqube {
-    properties {
-        property("sonar.projectKey", "all-stew_jam")
-        property("sonar.organization", "all-stew")
-        property("sonar.host.url", "https://sonarcloud.io")
-    }
-}
-
-tasks.register("clean-all-apps") {
+tasks.register("clean") {
     group = "build"
     description = "clean all apps"
     dependsOn(gradle.includedBuild("apps").task(":sample:clean"))
 }
 
-tasks.register("build-all-apps") {
+tasks.register("build") {
     group = "build"
     description = "Build all apps"
     dependsOn(gradle.includedBuild("apps").task(":sample:build"))
@@ -24,8 +12,14 @@ tasks.register("build-all-apps") {
 
 
 // This is an example of a lifecycle task that crosses build boundaries defined in the umbrella build.
-tasks.register("test-all-apps") {
+tasks.register("test") {
     group = "verification"
     description = "Run all apps tests"
     dependsOn(gradle.includedBuild("apps").task(":sample:test"))
+}
+
+tasks.register("sonar") {
+    group = "verification"
+    description = "send to sonar"
+    dependsOn(gradle.includedBuild("apps").task(":sample:sonar"))
 }
