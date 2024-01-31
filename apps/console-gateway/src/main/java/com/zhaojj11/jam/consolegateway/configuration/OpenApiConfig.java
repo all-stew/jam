d79780cf6dd747bfe627712f2adbc30c12321909
zhaojj11 <757355084@qq.com>
2024-02-01 00:06:03 +0800

    refactor(gateway): swagger and refactor

---
+++ apps/console-gateway/src/main/java/com/zhaojj11/jam/consolegateway/configuration/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.zhaojj11.jam.consolegateway.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * open api config
+ *
+ * @author zhaojj11
+ */
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI springOpenApi() {
+        return new OpenAPI().info(new Info()
+            .title("Console Gateway API")
+            .description("后台Api接口")
+            .version("1.0.0"));
+    }
+}
---
+++ apps/console-gateway/src/main/java/com/zhaojj11/jam/consolegateway/system/entity/vo/request/LoginUserRequestVO.java
@@ -0,0 +1,14 @@
+package com.zhaojj11.jam.consolegateway.system.entity.vo.request;
+
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * @author zhaojj11
+ */
+@Getter
+@ToString
+public class LoginUserRequestVO {
+    private String username;
+    private String password;
+}
